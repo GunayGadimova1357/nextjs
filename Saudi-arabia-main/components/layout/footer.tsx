ca76b073c69f169b48679fbba278c7071bc3ea8c
Gunay <gadimovagunay87@gmail.com>
2026-02-12 20:54:45 +0400

    commit

--- Saudi-arabia-main/components/layout/footer.tsx
+++ Saudi-arabia-main/components/layout/footer.tsx
@@ -1,32 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <footer className="relative z-20 shrink-0 border-t border-white/10 bg-[#1f1f22] text-white">
-            <div className="container mx-auto px-4 py-10 grid grid-cols-1 gap-10 md:grid-cols-4">
+            <div className="container mx-auto grid grid-cols-1 gap-10 px-4 py-10 md:grid-cols-4">
                 <div>
                     <Image src="/logo.png" alt="Saudi Welcome to Arabia" width={190} height={92} />
                 </div>
 
                 <div className="space-y-2">
                     <h4 className="text-xl font-semibold">About</h4>
-                    <p className="text-white/85">About us</p>
-                    <p className="text-white/85">Features</p>
-                    <p className="text-white/85">News & Blogs</p>
+                    <Link href="#" className="block text-white/85 hover:text-white">About us</Link>
+                    <Link href="#" className="block text-white/85 hover:text-white">Features</Link>
+                    <Link href="#" className="block text-white/85 hover:text-white">News & Blogs</Link>
                 </div>
 
                 <div className="space-y-2">
                     <h4 className="text-xl font-semibold">Contact</h4>
-                    <p className="text-white/85">Instagram</p>
-                    <p className="text-white/85">Twitter</p>
-                    <p className="text-white/85">Facebook</p>
+                    <Link href="#" className="block text-white/85 hover:text-white">Instagram</Link>
+                    <Link href="#" className="block text-white/85 hover:text-white">Twitter</Link>
+                    <Link href="#" className="block text-white/85 hover:text-white">Facebook</Link>
                 </div>
 
                 <div className="space-y-2">
                     <h4 className="text-xl font-semibold">Support</h4>
-                    <p className="text-white/85">FAQs</p>
-                    <p className="text-white/85">Support Centre</p>
-                    <p className="text-white/85">Feedback</p>
+                    <Link href="#" className="block text-white/85 hover:text-white">FAQs</Link>
+                    <Link href="#" className="block text-white/85 hover:text-white">Support Centre</Link>
+                    <Link href="#" className="block text-white/85 hover:text-white">Feedback</Link>
                 </div>
             </div>
         </footer>
